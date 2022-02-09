370f7f033611dfe9366502bd862801e1f80aadf5
unknown <nickmuoz>
2022-02-08 22:26:04 -0500

    add category in model products

--- src/models/products.js
+++ src/models/products.js
@@ -9,7 +9,7 @@ const productSchema = new Schema({
     reference: String,
     value:Number,
     description:String,
-    avaliability:Boolean,
+    available:Boolean,
     serialNumber:Number,
     quantity:Number,
 });
