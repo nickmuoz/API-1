12786bfe0962763156cecb4117c23b4e2624f0cc
unknown <nickmuoz>
2022-02-20 18:01:22 -0500

    backend update

--- src/models/customers.js
+++ src/models/customers.js
@@ -5,9 +5,14 @@ const Schema = mongoose.Schema;
 const customerSchema =  new Schema({
     name: String,
     password: String,
-    email: String, 
+    adress: String,
+    sex:String,
+    email: {
+        type:String,
+        require:true,
+    },
+    birthday: Date,
     phone: Number,
-    address: String,
 });
 
 module.exports = mongoose.model('customer', customerSchema);
