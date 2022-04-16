13a13509f1f3465be1d473433fb982692dedbf33
unknown <nickmuoz>
2022-04-16 15:51:26 -0500

    update user login end points

--- src/services/auth.service.js
+++ src/services/auth.service.js
@@ -6,16 +6,27 @@ const authService = {
     login: async function(data){
         try {
             const {email,password} = data;
-            let userExists = await User.findOne({email: email},'name email password').exec()
+            let userExists = await User.findOne({email: email},'name email password address type phone sex').exec()
             if (await bcrypt.compare(password,userExists.password).then(res=>res)){
                 const token = await this.signToken(userExists.id)
                 userExists.password = ''
                 const name = await userExists.name
+                const id = await userExists._id
+                const address = await userExists.address
+                const type = await userExists.type
+                const phone = await userExists.phone
+                const sex = await userExists.sex 
+
                 return {
                     code:200,
                     token: token,
                     email: email,
-                    name: name
+                    name: name,
+                    id: id,
+                    address: address,
+                    type: type,
+                    phone: phone,
+                    sex : sex
                 }
             }else{
                 return {
